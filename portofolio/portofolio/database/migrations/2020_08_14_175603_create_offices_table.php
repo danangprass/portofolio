aebefdfcffbefff600291b9d57d58ef980f4dd0b
Danang Prasetyo <dananng@outlook.com>
2020-08-15 14:08:36 +0700

    initial commit

---
+++ portofolio/portofolio/database/migrations/2020_08_14_175603_create_offices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfficesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('office_name');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offices');
+    }
+}
